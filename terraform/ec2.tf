7a158678160c1f70fb317ac4ec79496bebce9ff4
James Santucci <james.santucci@gmail.com>
2021-09-03 14:12:12 -0600

    Remove custom VPC config since it was broken

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -8,9 +8,6 @@ resource "aws_instance" "notebook_server" {
   associate_public_ip_address = true
   count                       = var.instance_count
 
-  # vpc_security_group_ids = [aws_security_group.notebook_server_sg.id]
-  # subnet_id              = aws_subnet.notebook_server_subnet.id
-
   tags = {
     Name = "Notebook server"
   }
@@ -21,51 +18,6 @@ resource "aws_key_pair" "notebook_server_key_pair" {
   key_name   = "foss4g_notebook_server_key"
 }
 
-resource "aws_vpc" "notebook_server_vpc" {
-  cidr_block = "10.0.0.0/16"
-}
-
-# The notebook server needs to accept ssh connections on the standard
-# port (for configuration) and TCP connections on :8888 for notebook
-# connections
-# It needs to allow outbound access to the Raster Foundry API and
-# I think nothing else
-resource "aws_security_group" "notebook_server_sg" {
-  vpc_id = aws_vpc.notebook_server_vpc.id
-}
-
-resource "aws_security_group_rule" "notebook_server_ssh" {
-  type             = "ingress"
-  from_port        = 22
-  to_port          = 22
-  protocol         = "tcp"
-  cidr_blocks      = ["0.0.0.0/0"]
-  ipv6_cidr_blocks = ["::/0"]
-
-  security_group_id = aws_security_group.notebook_server_sg.id
-}
-
-resource "aws_security_group_rule" "notebook_server_jupyter_8888" {
-  type             = "ingress"
-  from_port        = 8888
-  to_port          = 8888
-  protocol         = "tcp"
-  cidr_blocks      = ["0.0.0.0/0"]
-  ipv6_cidr_blocks = ["::/0"]
-
-  security_group_id = aws_security_group.notebook_server_sg.id
-}
-
-resource "aws_subnet" "notebook_server_subnet" {
-  vpc_id            = aws_vpc.notebook_server_vpc.id
-  cidr_block        = "10.0.10.0/24"
-  availability_zone = "us-east-1a"
-}
-
 output "instance_ips" {
-  value = aws_instance.notebook_server.*.ipv6_address_count
-}
-
-output "security_group_id" {
-  value = aws_security_group.notebook_server_sg.id
+  value = aws_instance.notebook_server.public_ip
 }
