1f3a9d1bf627f04f2d90d9437424f903e9c40584
James Santucci <james.santucci@gmail.com>
2021-09-06 20:32:47 -0600

    Choose different instance type, attach volume as separate resource

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,11 +1,11 @@
 resource "aws_instance" "notebook_server" {
-  # Ubuntu 20.04, 2021-08-25
-  ami = "ami-03b5637245555e13d"
-  # 4 cpu, 8gb memory, decent sized ssd
-  instance_type = "c6gd.xlarge"
+  # 4 cpu, 8gb memory, no nvme (have to attach block store separately)
+  instance_type = "c6g.xlarge"
+  ami           = "ami-03b5637245555e13d"
 
   key_name                    = aws_key_pair.notebook_server_key_pair.key_name
   associate_public_ip_address = true
+  availability_zone           = "us-east-1a"
   count                       = var.instance_count
 
   tags = {
@@ -16,6 +16,19 @@ resource "aws_instance" "notebook_server" {
 resource "aws_key_pair" "notebook_server_key_pair" {
   public_key = var.notebook_server_public_key
   key_name   = "foss4g_notebook_server_key"
+}
+
+resource "aws_ebs_volume" "data_volume" {
+  size              = 128
+  availability_zone = "us-east-1a"
+  count             = var.instance_count
+}
+
+resource "aws_volume_attachment" "data_volume_attachment" {
+  device_name = "/dev/sdh"
+  for_each    = { for idx in range(var.instance_count) : idx => tostring(idx) }
+  volume_id   = aws_ebs_volume.data_volume[each.key].id
+  instance_id = aws_instance.notebook_server[each.key].id
 }
 
 output "instance_ips" {
