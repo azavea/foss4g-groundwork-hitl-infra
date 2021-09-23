17cb541aeea6316fe197da5a1f9c183a5e776b26
James Santucci <james.santucci@gmail.com>
2021-09-23 15:45:29 -0600

    fix security group rules for servers

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,7 +1,8 @@
 resource "aws_instance" "notebook_server" {
   # 4 cpu, 8gb memory, no nvme (have to attach block store separately)
-  instance_type = "c6g.xlarge"
-  ami           = "ami-03b5637245555e13d"
+  instance_type          = "c5a.xlarge"
+  ami                    = "ami-03a80f322a6053f85"
+  vpc_security_group_ids = [ aws_security_group.notebook_server_sg.id ]
 
   key_name                    = aws_key_pair.notebook_server_key_pair.key_name
   associate_public_ip_address = true
@@ -11,6 +12,31 @@ resource "aws_instance" "notebook_server" {
   tags = {
     Name = "Notebook server"
   }
+}
+
+resource "aws_security_group" "notebook_server_sg" {
+  name = "notebook server security group"
+  revoke_rules_on_delete = true
+}
+
+resource "aws_security_group_rule" "allow_8888" {
+  type = "ingress"
+  from_port = 8888
+  to_port = 8888
+  protocol = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+  ipv6_cidr_blocks = ["::/0"]
+  security_group_id = aws_security_group.notebook_server_sg.id
+}
+
+resource "aws_security_group_rule" "allow_ssh" {
+  type = "ingress"
+  from_port = 22
+  to_port = 22
+  protocol = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+  ipv6_cidr_blocks = ["::/0"]
+  security_group_id = aws_security_group.notebook_server_sg.id
 }
 
 resource "aws_key_pair" "notebook_server_key_pair" {
